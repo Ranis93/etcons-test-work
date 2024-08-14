c0f21e26914fe7190b8833351eee8115b0b56f9b
Ranis <ranis.krd@gmail.com>
2024-08-14 23:47:44 +0300

    fix: three commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  base: '/etcons-test-work',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
